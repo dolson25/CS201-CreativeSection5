cdbd270b156bc234b3ad1ba9f4524f9a23d56189
Dallin Olson <dallin.olson@gmail.com>
2017-03-29 01:23:03 +0000

    questions finishedish

--- public/javascripts/questions.js
+++ public/javascripts/questions.js
@@ -4,6 +4,30 @@ $(document).ready(function() {
             $("#q2").fadeIn();
         });
     });
-    
+    $("#sportSubmit").click(function() {
+        $("#q2").hide("slow", function() {
+            $("#q3").fadeIn();
+        });
+    });
+    $("#siblingSubmit").click(function() {
+        $("#q3").hide("slow", function() {
+            $("#q4").fadeIn();
+        });
+    });
+    $("#skillsSubmit").click(function() {
+        $("#q4").hide("slow", function() {
+            $("#q5").fadeIn();
+        });
+    });
+    $("#commentSubmit").click(function() {
+        $("#q5").hide("slow", function() {
+           $("body").css("background-color","white");
+            $("h1").text("Here are the Results:");
+        });
+    });
     
 });
+
+function outputUpdate(vol) {
+        document.querySelector('#sliderValue').value = vol;
+    }
